58c2e91c65b49f051b803831e82b903a27551548
MatthewChristofas <tuj80541@temple.edu>
2024-03-19 10:47:21 -0400

    Added Park Safety webpage

--- exploring-national-parks/src/GlobalComponents/Navbar.jsx
+++ exploring-national-parks/src/GlobalComponents/Navbar.jsx
@@ -31,6 +31,9 @@ const Navbar = () => {
                 <li>
                     <NavLink to="/ParkPlan">Park Planner</NavLink>
                 </li>
+                <li>
+                    <NavLink to="/ParkSafety">Park Safety</NavLink>
+                </li>
             </ul>
         </nav>
     )
---
+++ exploring-national-parks/src/ParkSafety.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ParkInfoComponent from './ParkInfo/Components/ParkInfoComponent.jsx';
+import './Style/ParkSafety.css';
+/**
+ * Renders the ParkSafety component page...
+ * 
+ * @component
+ * @module ParkSafety
+ * 
+ * @returns {JSX.Element} The rendered ParkInfo component.
+ */
+function ParkSafety(){
+    return(
+        <div className="park-info-parent">
+            <ParkInfoComponent />
+        </div>
+    );
+}
+
+export default ParkInfo;
