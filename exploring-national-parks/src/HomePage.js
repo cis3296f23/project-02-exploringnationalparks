6fa0d5957a91061e36935fad66240c42b3243030
Andriy Luchko <tul33640@temple.edu>
2024-10-21 22:18:48 -0400

    Added temple twitter posts to bottom of homepage

--- exploring-national-parks/src/HomePage.js
+++ exploring-national-parks/src/HomePage.js
@@ -10,6 +10,7 @@ import Buttons from './HomePage/Components/Buttons'
 import yosemite from './HomePage/Assets/yosemite.jpg';
 import './Style/homepage.css'
 import HighlightGallery from './HomePage/Components/HighlightGallery';
+import TwitterTimeline from './TempleTimeline/Components/TempleTimeline';
 const HomePage = () => {
   return (
     // <Navbar/>
@@ -18,6 +19,7 @@ const HomePage = () => {
         <Welcome/>
         <HighlightGallery/>
         <Buttons/>
+        <TwitterTimeline/>
     </div>
   )
 }
