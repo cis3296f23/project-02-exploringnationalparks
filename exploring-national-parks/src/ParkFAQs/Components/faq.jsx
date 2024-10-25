989e51effcf3b4f793027565be48cf42dfc52dfb
ahgoldmeer <ahgoldmeer@gmail.com>
2024-10-25 10:53:13 -0400

    Added FAQ page, adjusted index to properly add navigation

---
+++ exploring-national-parks/src/ParkFAQs/Components/faq.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+/**
+ * Component representing the welcome section of the homepage.
+ * @module FAQ
+ * @memberof ParkFAQs
+ * @returns {JSX.Element} The rendered welcome section.
+ */
+const FAQ = () => {
+    return (
+        <div className = "FAQ">
+            <h1 className = "FAQs">National Park Service FAQs</h1>
+            <p className = "FAQs">
+               This is a test. This should display on the FAQs page
+            </p>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default FAQ
