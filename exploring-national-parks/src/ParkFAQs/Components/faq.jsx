5c55cfff04219f1826fa8a76801332f19c445a5c
ahgoldmeer <ahgoldmeer@gmail.com>
2024-10-25 11:12:17 -0400

    Added dropdown, added FAQ tabs

--- exploring-national-parks/src/ParkFAQs/Components/faq.jsx
+++ exploring-national-parks/src/ParkFAQs/Components/faq.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Dropdown from './Dropdown'
 
 /**
  * Component representing the welcome section of the homepage.
@@ -11,7 +12,26 @@ const FAQ = () => {
         <div className = "FAQ">
             <h1 className = "FAQs">National Park Service FAQs</h1>
             <p className = "FAQs">
-               This is a test. This should display on the FAQs page
+               <Dropdown
+                    question="Who is the director of the National Park Service?"
+                    answer="Charles F. 'Chuck' Sams III is the director of the National Park Service."
+                />
+                <Dropdown
+                    question="What government agency oversees the National Park Service?"
+                    answer="The National Park Service is a bureau of the Department of the Interior. Directly overseeing its operation is the department's Assistant Secretary for Fish, Wildlife and Parks."
+                />
+                <Dropdown
+                    question="How many employees are in the National Park Service?"
+                    answer="Approximately 20,000 full time employees, and over 279,000 volunteers"
+                />
+                <Dropdown
+                    question="How old is the National Park System?"
+                    answer="The National Park Service was created by an act signed by President Woodrow Wilson on August 25, 1916. Yellowstone National Park was established by an act signed by President Ulysses S. Grant on March 1, 1872, as the nation's first national park."
+                />
+                <Dropdown
+                    question="How many people visit the national parks?"
+                    answer="In 2022, the total number of recreation visits to the national parks was 311,985,998."
+                />
             </p>
             <div>
 
