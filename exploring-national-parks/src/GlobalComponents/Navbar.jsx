f0dd9cacdf9208e9914506c7a1883ab52cad725e
I584999 <will.sims@sap.com>
2024-10-25 10:16:46 -0400

    New NavLink in Header

--- exploring-national-parks/src/GlobalComponents/Navbar.jsx
+++ exploring-national-parks/src/GlobalComponents/Navbar.jsx
@@ -31,6 +31,9 @@ const Navbar = () => {
                 <li>
                     <NavLink to="/ParkPlan">Park Planner</NavLink>
                 </li>
+                <li>
+                    <NavLink to="/ParkFAQs">FAQs</NavLink>
+                </li>
             </ul>
         </nav>
     )
