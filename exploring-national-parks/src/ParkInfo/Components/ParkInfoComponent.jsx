c01a34c4d859d805375755a36ab08eb959031cb8
molly-pop <157141271+molly-pop@users.noreply.github.com>
2024-10-28 15:01:08 -0400

    Adding further css and inlien styling to weather components

--- exploring-national-parks/src/ParkInfo/Components/ParkInfoComponent.jsx
+++ exploring-national-parks/src/ParkInfo/Components/ParkInfoComponent.jsx
@@ -101,15 +101,27 @@ function ParkInfoComponent() {
                                     <img src={park.images.length !== 0  ? park.images[0].url : ''} alt='' width='100' height='300'/>
                             </div>
                             <p className="description">{park.description}</p>
-                            <div className="weather">
-                            <p className="description">Current weather: {park.weather.current.temperature_2m} °C</p>
-                            <img src = {(park.weather.current.weather_code) == 0 ? sunIcon :
-                                [71, 73, 75, 77, 85, 86].includes(park.weather.current.weather_code)? snowflakeIcon : 
-                                [95, 96, 99].includes(park.weather.current.weather_code)? stormIcon : 
-                                [51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82].includes(park.weather.current.weather_code)? rainIcon : 
-                                cloudIcon} className="weatherIcon" />
-                            </div>
-                            
+                                <div
+                                    className={`weatherContainer ${
+                                        (park.weather.current.weather_code === 0) ? 'sunny' :
+                                            [71, 73, 75, 77, 85, 86].includes(park.weather.current.weather_code) ? 'snowy' :
+                                                [95, 96, 99].includes(park.weather.current.weather_code) ? 'stormy' :
+                                                    [51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82].includes(park.weather.current.weather_code) ? 'rainy' :
+                                                        'cloudy'
+                                    }`}
+                                >
+                                    <p className="description">Current weather: {park.weather.current.temperature_2m} °C</p>
+                                    <img
+                                        src={
+                                            (park.weather.current.weather_code === 0) ? sunIcon :
+                                                [71, 73, 75, 77, 85, 86].includes(park.weather.current.weather_code) ? snowflakeIcon :
+                                                    [95, 96, 99].includes(park.weather.current.weather_code) ? stormIcon :
+                                                        [51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82].includes(park.weather.current.weather_code) ? rainIcon :
+                                                            cloudIcon
+                                        }
+                                        className="weatherIcon"
+                                    />
+                                </div>
                             </a>
                         </div>
                         ))}
