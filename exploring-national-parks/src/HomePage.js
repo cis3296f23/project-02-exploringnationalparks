fcc24c70d5bf7b348ff9a42ba879a6e24b83f8e1
JasonLahodaTU <93489427+JPLahoda@users.noreply.github.com>
2024-10-30 20:19:48 -0400

    added weather API display
    added weather API display on home screen

--- exploring-national-parks/src/HomePage.js
+++ exploring-national-parks/src/HomePage.js
@@ -4,22 +4,52 @@
  * @module HomePage
  * @returns {JSX.Element} The rendered home page component.
  */
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import Welcome from './HomePage/Components/Welcome'
 import Buttons from './HomePage/Components/Buttons'
 import yosemite from './HomePage/Assets/yosemite.jpg';
 import './Style/homepage.css'
 import HighlightGallery from './HomePage/Components/HighlightGallery';
-const HomePage = () => {
-  return (
-    // <Navbar/>
-    <div className = "home-page main-component">
-        {/* <h1>Test Hello</h1> */}
-        <Welcome/>
-        <HighlightGallery/>
-        <Buttons/>
-    </div>
-  )
-}
+import axios from 'axios';
 
-export default HomePage
+const HomePage = () => {
+
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const apiKey = '057e459b323be812a262c65a65f78048';
+      const lat = 39.952583; // Latitude for Philadelphia
+      const lon = -75.165222; // Longitude for Philadelphia
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+
+      try {
+        const response = await axios.get(url);
+        setWeather(response.data);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    };
+
+    fetchWeather();
+  }, []);
+
+  return (
+      <div className="home-page main-component">
+        <h2>Welcome to the National Parks Explorer</h2>
+        {weather ? (
+          <div id="weather-container">
+            <h3>Current Weather</h3>
+            <p>{`${weather.name}: ${weather.main.temp}°F, ${weather.weather[0].description}`}</p>
+          </div>
+        ) : (
+          <p>Loading weather data...</p>
+        )}
+        <Welcome />
+        <HighlightGallery />
+        <Buttons />
+      </div>
+    );
+  };
+
+export default HomePage;
